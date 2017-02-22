b196756f08369653930ebe655b0731edd4de6346
Spencer <sta7ey@virginia.edu>
2017-02-21 19:49:19 -0500

    Reverse columns and rows

--- KeyPad/KeyPad.cpp
+++ KeyPad/KeyPad.cpp
@@ -17,20 +17,23 @@ KeyPad::~KeyPad(){
 int KeyPad::checkKeyPress(){
 
 	// loop through columns and set each pin to input
-	for (int c = 0; c < 3; c++) {
-		check->setPin(c);
+	// cannot use pins 0 or 1
+	for (int r = 0; r < 4; r++) {
+		int output = r + 7;
+		digitalWrite(output, HIGH);
 
 		// loop through rows and set each pin to output 5V
-		for (int r = 0; r < 4; r++) {
-			int pin2 = r + 3;
-			digitalWrite(pin2, HIGH);
+		for (int c = 0; c < 3; c++) {
+			int input = c + 4;
+			check->setPin(input);
 
 			// determine which button was pressed
 			if (check->checkButtonEvent()){
-				return c + 1 + 3 * r;
+				digitalWrite(output, LOW);
+				return 1 + c + 3 * r;
 			}
-
 		}
+		digitalWrite(output, LOW);
 	}
 
 	// if no button was pressed
