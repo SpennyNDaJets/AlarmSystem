c3178bfba96d1aea0157f402713814f533887612
Spencer <sta7ey@virginia.edu>
2017-02-22 21:00:08 -0500

    Syntax Error Fix

--- Validator/Validator.cpp
+++ Validator/Validator.cpp
@@ -2,3 +2,80 @@
 
 #include "Validator.h"
 
+// default constructor
+Validator::Validator() {
+	for (int i = 0; i < 4; i++){
+		code[i] = 0;
+	}
+	count = 0;
+}
+
+// constructor with 4 ints
+Validator::Validator(int f, int s, int t, int fo) {
+	code[0] = f;
+	code[1] = s;
+	code[2]	= t;
+	code[3] = fo;
+	count = 0;
+}
+
+// convert to 0 if zero key is pressed
+int Validator::convertZero(int digit) {
+	if (digit == zero)
+		return 0;
+	else
+		return digit;
+}
+
+// reset if '*' was pressed
+void Validator::reset() {
+	count = 0;
+}
+
+// check if entered code was correct
+bool Validator::validateCode() {
+	if (count == 4) {
+		count = 0;
+		return true;
+	}
+	else {
+		count = 0;
+		return false;
+	}
+}
+
+
+// check if digit matches code and unlock if all digits entered followed by '#'
+bool Validator::checkCode(int digit){
+	// convert if 0
+	int enter = convertZero(digit);
+
+	// check '*'
+	if (enter == star){
+		this->reset();
+		return false;
+	}
+
+	// validate code if '#'
+	else if (enter == pound){
+		return this->validateCode();
+	}
+
+
+	// code is wrong if counter is less than 0 or greater than 3
+	if ((count < 0) || (count > 3)) {
+		return false;
+	}
+	
+	// check if button matches next digit of code
+	else if (code[count] == enter) {
+		count ++;
+		return false;
+	}
+
+	// code is wrong and set counter to -1
+	else {
+		count = -1;
+		return false;
+	}
+}
--- Validator/Validator.h
+++ Validator/Validator.h
@@ -1,22 +1,38 @@
-// validator class
+// Validator class
 
 #ifndef VALIDATOR_H
 #define VALIDATOR_H
 
-#include "arduino.h"
-#include "KeyPad.h"
-
 
 class Validator {
 public:
+	// constants
+	const int star = 10;
+	const int zero = 11;
+	const int pound = 12;
+
+	// default constructor
 	Validator();
-	Validator(int f, int s, int t, int f);
-	~Validator();
-	bool checkDigit(int position, int digit);
-	bool unlock();
+
+	// constructor with 4 inputs
+	Validator(int f, int s, int t, int fo);
+
+	// convert to 0 if zero key is pressed
+	int convertZero(int digit);
+
+	// reset if star was pressed
+	void reset();
+
+	//check if entered code was correct
+	bool validateCode();
+
+	// check if digit matches code and unlock if all digits entered followed by '#'
+	bool checkCode(int digit);
+
 
 private:
-	int[]* code;
+	int code[4];
+	int count;
 };
 
 #endif
